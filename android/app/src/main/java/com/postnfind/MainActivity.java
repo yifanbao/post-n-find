3b91ca1a31ca6d2947c4de2b3448c60753f6ea57
yifann <yifanbao@outlook.com>
2019-09-06 15:58:04 -0500

    Add splash screen

--- android/app/src/main/java/com/postnfind/MainActivity.java
+++ android/app/src/main/java/com/postnfind/MainActivity.java
@@ -1,5 +1,11 @@
 package com.postnfind;
 
+import android.widget.LinearLayout;
+import android.graphics.Color;
+import android.widget.TextView;
+import android.view.Gravity;
+import android.util.TypedValue;
+
 import com.facebook.react.ReactActivity;
 import com.reactnativenavigation.controllers.SplashActivity;
 import com.imagepicker.permissions.OnImagePickerPermissionsCallback;
@@ -7,6 +13,23 @@ import com.facebook.react.modules.core.PermissionListener;
 
 public class MainActivity extends SplashActivity implements OnImagePickerPermissionsCallback {
   private PermissionListener listener;
+
+  @Override
+  public LinearLayout createSplashLayout() {
+    LinearLayout view = new LinearLayout(this);
+    TextView textView = new TextView(this);
+
+    view.setBackgroundColor(Color.parseColor("#FFFFFF"));
+    view.setGravity(Gravity.CENTER);
+
+    textView.setBackgroundColor(Color.parseColor("#3C00DF"));
+    textView.setText("PostNFind");
+    textView.setGravity(Gravity.CENTER);
+    textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 40);
+
+    view.addView(textView);
+    return view;
+  }
 
   @Override
   public void setPermissionListener(PermissionListener listener)
